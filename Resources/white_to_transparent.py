cf2a916a2e666b9389775c988974b014ba452ef3
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-11-01 14:44:02 +0100

    Add new stuff

--- Resources/white_to_transparent.py
+++ Resources/white_to_transparent.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageSequence
 
 # Open the GIF
-img = Image.open("baldi-gif.gif")
+img = Image.open("Resources/baldi-happy.gif")
 
 # Process each frame
 frames = []
@@ -15,7 +15,7 @@ for frame in ImageSequence.Iterator(img):
     for item in datas:
         # Change all white (also shades of whites)
         # pixels to transparent
-        if item[0] > 200 and item[1] > 200 and item[2] > 200:
+        if item[0] > 222 and item[1] > 222 and item[2] > 222:
             new_data.append((255, 255, 255, 0))  # Set transparency
         else:
             new_data.append(item)
