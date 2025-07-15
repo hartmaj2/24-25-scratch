d602103d453dcd216beac047b7678bbfe44438e8
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-07-15 14:04:17 +0200

    Comment and polish code

--- Resources/if_trigger_test.py
+++ Resources/if_trigger_test.py
@@ -1,33 +1,40 @@
+# A common mistakes that students do in Scratch is, that when they want to pick uniformly at random from n possibilities they:
+# Create n if blocks and put into each (pick random (1) to (n))
+# Then they hope that this does what it should
+# But some cases, this will not trigger any if or trigger more than one!
+
+# The program below shows you the ratio, how many times the program will correctly select exactly one possibility out of 1,...,n ifs
+
 import random
 import matplotlib.pyplot as plt
 
-n_of_ifs_limit = 20
+n = 20
 
-no_of_reps = 100_000
+trials = 100_000
 
-ifs= []
-ratios = []
+ifs = []
+success_ratio = []
 
-for ifs_count in range(2,n_of_ifs_limit):
+for ifs_count in range(2,n):
     successes = 0
-    for i in range(no_of_reps):
+    for i in range(trials):
         triggers = 0
         for j in range(1,ifs_count+1):
             if random.randint(1,ifs_count) == j:
                 triggers += 1
-        if triggers == 1:
+        if triggers == 1: # check if exactly one if was triggered 
             successes += 1
     ifs.append(ifs_count)
-    ratios.append(successes/no_of_reps)
+    success_ratio.append(successes/trials)
 print(f"Ifs: {ifs}")
-print(f"Ratios: {ratios}")
+print(f"Ratios: {success_ratio}")
 
-plt.scatter(ifs,ratios)
+plt.scatter(ifs,success_ratio)
 
-for i in range(len(ratios)):
-    plt.text(ifs[i],ratios[i],f"{ratios[i]:.3}",ha="left",va="bottom")
+for i in range(len(success_ratio)):
+    plt.text(ifs[i],success_ratio[i],f"{success_ratio[i]:.3}",ha="left",va="bottom")
 
 
-plt.xticks(range(0,n_of_ifs_limit))
+plt.xticks(range(0,n))
 plt.yticks([x * 0.1 for x in range(0,10)])
 plt.show()
